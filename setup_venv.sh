61b2452a7b0c192ccefde375d5332566654c42b9
lollo004 <g.sighing380@passmail.com>
2026-02-18 09:46:10 +0000

    add python venv setup script

---
+++ setup_venv.sh
@@ -0,0 +1,18 @@
+cd /workspace/pulpy
+
+uv venv
+source .venv/bin/activate
+
+uv pip install torch torchvision
+
+cd /workspace/pulpy/code/Umamba2
+
+uv pip install -e .
+uv pip install wheel
+uv pip install causal-conv1d==1.5.2 mamba-ssm==2.2.5 --no-build-isolation
+
+uv pip install -U "transformers==4.47.1"
+
+cd /workspace/pulpy
+
+echo "READY."
